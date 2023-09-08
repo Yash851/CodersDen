48afb5e00827d79306d6c078861db116d3f20616
Yash Gandhi <64217975+Yash851@users.noreply.github.com>
2023-09-08 15:07:33 -0400

    Delete CommentFunctions.js

--- CommentFunctions.js
+++
@@ -1,9 +0,0 @@
-import db from "./db.js";
-
-export function getPostComments(postId){
-return db
-    .select('*')
-    .from('comments')
-    .where({questionid:postId})
-    .join('users', 'users.userid', '=', 'comments.userid')
-}
