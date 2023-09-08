a99552070b4cb7625457a1b989d8b22efdc1dcb1
Yash Gandhi <64217975+Yash851@users.noreply.github.com>
2023-09-08 15:07:06 -0400

    Delete AnswerFunctions.js

--- AnswerFunctions.js
+++
@@ -1,9 +0,0 @@
-import db from "./db.js";
-
-export function getPostAnswers(postId){
-return db
-    .select('*')
-    .from('answers')
-    .where({questionid:postId})
-    .join('users', 'users.userid', '=', 'answers.userid')
-}
